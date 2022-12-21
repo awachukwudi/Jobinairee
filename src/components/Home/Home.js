f844e6d1c952c7123c9cfb2ea39931300f965313
awachukwudi <111116458+awachukwudi@users.noreply.github.com>
2022-12-21 22:34:38 +0400

    company logos changed

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,9 @@
 import jobinairee from "../Images/HeroImage.png";
-import Amazon from "../Images/Amazonlogo.png";
-import Google from "../Images/Google.png";
-import Pinterest from "../Images/Pinterest.png";
-import Vector from "../Images/Vector.png";
-import WeChat from "../Images/WeChat.png";
+import Cowrywise from "../Images/cowrywise.png";
+import Uba from "../Images/uba.png";
+import Softcom from "../Images/softcom.png";
+import Piggyvest from "../Images/piggyvest.png";
+import Accessbank from "../Images/accessbank.png";
 import SoftwareEngineer from "../Images/softwareengineer.png";
 import Marketing from "../Images/marketing.png";
 import CustomerService from "../Images/customerservice.png";
@@ -16,6 +16,7 @@ import Registeraccount from "../Images/registeraccount.png";
 import Createresume from "../Images/createresume.png";
 import Findmatchjobs from "../Images/findmatchjobs.png";
 import Easyapply from "../Images/easyapply.png";
+import { Link } from "react-router-dom";
 
 export const Home = () => {
   return (
@@ -51,24 +52,30 @@ export const Home = () => {
           <h2 class="pt-4">Job opportunities from leading companies</h2>
         </div>
 
-        <div className="container">
+        <div className="icons-section container">
           <div class="icons-sec columns is-small">
             <div class="column">
-              <img src={Vector} alt="Bank-of-America-logo" />
+              <img src={Piggyvest} alt="Piggyvest-logo" />
             </div>
             <div class="column">
-              <img src={Pinterest} alt="Pinterest-logo" />
+              <img src={Softcom} alt="Softcom-logo" />
             </div>
             <div class="column">
-              <img src={Google} alt="Google-logo" />
+              <img src={Uba} alt="Uba-logo" />
             </div>
             <div class="column">
-              <img src={Amazon} alt="Amazon-logo" />
+              <img src={Cowrywise} alt="Cowrywise-logo" />
             </div>
             <div class="column">
-              <img src={WeChat} alt="WeChat-logo" />
+              <img src={Accessbank} alt="Accessbank-logo" />
             </div>
           </div>
+        </div>
+
+        <div class="first-section-bottom-text">
+          <Link>
+            <h2>View All Companies Hiring</h2>
+          </Link>
         </div>
       </section>
 
@@ -98,61 +105,69 @@ export const Home = () => {
         </div>
         <div className="container ">
           <div class="first-card-sec  container d-flex  mt-5 ">
-            <div class="row">
-              <div class="col-md-4">
-                <div class="card1 p-3 ">
-                  <div class="d-flex flex-row mb-3">
-                    <img src={Productmanager} alt="" width="40" />
-                  </div>
-                  <h6>Product Manager</h6>
-                  <div class="d-flex justify-content-between  mt-3">
-                    <span>57 Vacancy</span>
+            <Link>
+              <div class="row">
+                <div class="col-md-4">
+                  <div class="card1 p-3 ">
+                    <div class="d-flex flex-row mb-3">
+                      <img src={Productmanager} alt="" width="40" />
+                    </div>
+                    <h6>Product Manager</h6>
+                    <div class="d-flex justify-content-between  mt-3">
+                      <span>57 Vacancy</span>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
+            </Link>
 
-            <div class="row">
-              <div class="col-md-4">
-                <div class="card1 p-3">
-                  <div class="d-flex flex-row mb-3">
-                    <img src={Contentcreator} alt="" width="40" />
-                  </div>
-                  <h6>Content Creator</h6>
-                  <div class="d-flex justify-content-between install mt-3">
-                    <span>74 Vacancy</span>
+            <Link>
+              <div class="row">
+                <div class="col-md-4">
+                  <div class="card1 p-3">
+                    <div class="d-flex flex-row mb-3">
+                      <img src={Contentcreator} alt="" width="40" />
+                    </div>
+                    <h6>Content Creator</h6>
+                    <div class="d-flex justify-content-between install mt-3">
+                      <span>74 Vacancy</span>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
+            </Link>
 
-            <div class="row">
-              <div class="col-md-4">
-                <div class="card1 p-3">
-                  <div class="d-flex flex-row mb-3">
-                    <img src={Salesmarketing} alt="" width="40" />
-                  </div>
-                  <h6>Sales $ Marketing</h6>
-                  <div class="d-flex justify-content-between install mt-3">
-                    <span>126 Vacancy</span>
+            <Link>
+              <div class="row">
+                <div class="col-md-4">
+                  <div class="card1 p-3">
+                    <div class="d-flex flex-row mb-3">
+                      <img src={Salesmarketing} alt="" width="40" />
+                    </div>
+                    <h6>Sales $ Marketing</h6>
+                    <div class="d-flex justify-content-between install mt-3">
+                      <span>126 Vacancy</span>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
+            </Link>
 
-            <div class="row">
-              <div class="col-md-4">
-                <div class="card1 p-3">
-                  <div class="d-flex flex-row mb-3">
-                    <img src={Businessdeveloper} alt="" width="40" />
-                  </div>
-                  <h6>Business Developer</h6>
-                  <div class="d-flex justify-content-between install mt-3">
-                    <span>205 Vacancy</span>
+            <Link>
+              <div class="row">
+                <div class="col-md-4">
+                  <div class="card1 p-3">
+                    <div class="d-flex flex-row mb-3">
+                      <img src={Businessdeveloper} alt="" width="40" />
+                    </div>
+                    <h6>Business Developer</h6>
+                    <div class="d-flex justify-content-between install mt-3">
+                      <span>205 Vacancy</span>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
+            </Link>
           </div>
         </div>
 
@@ -160,62 +175,76 @@ export const Home = () => {
 
         <div className="container ">
           <div class="first-card-sec  container d-flex  mt-1 ">
-            <div class="row">
-              <div class="col-md-4">
-                <div class="card1 p-3 ">
-                  <div class="d-flex flex-row mb-3">
-                    <img src={Marketing} alt="" width="40" />
-                  </div>
-                  <h6>Marketing</h6>
-                  <div class="d-flex justify-content-between  mt-3">
-                    <span>142 Vacancy</span>
+            <Link>
+              <div class="row">
+                <div class="col-md-4">
+                  <div class="card1 p-3 ">
+                    <div class="d-flex flex-row mb-3">
+                      <img src={Marketing} alt="" width="40" />
+                    </div>
+                    <h6>Marketing</h6>
+                    <div class="d-flex justify-content-between  mt-3">
+                      <span>142 Vacancy</span>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
+            </Link>
 
-            <div class="row">
-              <div class="col-md-4">
-                <div class="card1 p-3">
-                  <div class="d-flex flex-row mb-3">
-                    <img src={CustomerService} alt="" width="40" />
-                  </div>
-                  <h6>Customer Service</h6>
-                  <div class="d-flex justify-content-between install mt-3">
-                    <span>42 Vacancy</span>
+            <Link>
+              <div class="row">
+                <div class="col-md-4">
+                  <div class="card1 p-3">
+                    <div class="d-flex flex-row mb-3">
+                      <img src={CustomerService} alt="" width="40" />
+                    </div>
+                    <h6>Customer Service</h6>
+                    <div class="d-flex justify-content-between install mt-3">
+                      <span>42 Vacancy</span>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
+            </Link>
 
-            <div class="row">
-              <div class="col-md-4">
-                <div class="card1 p-3">
-                  <div class="d-flex flex-row mb-3">
-                    <img src={SoftwareEngineer} alt="" width="40" />
-                  </div>
-                  <h6>Software Engineer</h6>
-                  <div class="d-flex justify-content-between install mt-3">
-                    <span>88 Vacancy</span>
+            <Link>
+              <div class="row">
+                <div class="col-md-4">
+                  <div class="card1 p-3">
+                    <div class="d-flex flex-row mb-3">
+                      <img src={SoftwareEngineer} alt="" width="40" />
+                    </div>
+                    <h6>Software Engineer</h6>
+                    <div class="d-flex justify-content-between install mt-3">
+                      <span>88 Vacancy</span>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
+            </Link>
 
-            <div class="row">
-              <div class="col-md-4">
-                <div class="card1 p-3">
-                  <div class="d-flex flex-row mb-3">
-                    <img src={Productdesigner} alt="" width="40" />
-                  </div>
-                  <h6>Product Designer</h6>
-                  <div class="d-flex justify-content-between install mt-3">
-                    <span>105 Vacancy</span>
+            <Link>
+              <div class="row">
+                <div class="col-md-4">
+                  <div class="card1 p-3">
+                    <div class="d-flex flex-row mb-3">
+                      <img src={Productdesigner} alt="" width="40" />
+                    </div>
+                    <h6>Product Designer</h6>
+                    <div class="d-flex justify-content-between install mt-3">
+                      <span>105 Vacancy</span>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
+            </Link>
           </div>
+        </div>
+
+        <div class="first-section-bottom-text">
+          <Link>
+            <h2>Explore Other Categories</h2>
+          </Link>
         </div>
       </section>
 
@@ -225,72 +254,82 @@ export const Home = () => {
         <div className="second-section-top-div-text">
           <h2>How it works</h2>
         </div>
+
         <div className="second-card-section">
           <div class="second-card container d-flex d-flex-row mt-5">
-            <div class="row">
-              <div class="col-md-4">
-                <div class="card p-3">
-                  <div class="d-flex flex-row mb-3">
-                    <img src={Registeraccount} alt="" width="40" />
-                  </div>
-                  <h6>Register Account</h6>
-                  <div class="Register-account-text d-flex justify-content-between install mt-3">
-                    <span>
-                      Register Account with few steps and get work opportunities
-                    </span>
+            <Link>
+              <div class="row">
+                <div class="col-md-4">
+                  <div class="card  p-3">
+                    <div class="d-flex flex-row  mb-3">
+                      <img src={Registeraccount} alt="" width="40" />
+                    </div>
+                    <h6>Register Account</h6>
+                    <div class="Register-account-text d-flex justify-content-between install mt-3">
+                      <span>
+                        Register Account with few steps and get work
+                        opportunities
+                      </span>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
+            </Link>
 
-            <div class="row">
-              <div class="col-md-4">
-                <div class="card p-3">
-                  <div class="d-flex flex-row mb-3">
-                    <img src={Createresume} alt="" width="40" />
-                  </div>
-                  <h6>Create Resume</h6>
-                  <div class="d-flex justify-content-between install mt-3">
-                    <span>
-                      Create a resume on Jobineer and get hired easily
-                    </span>
+            <Link>
+              <div class="row">
+                <div class="col-md-4">
+                  <div class="card p-3">
+                    <div class="d-flex flex-row mb-3">
+                      <img src={Createresume} alt="" width="40" />
+                    </div>
+                    <h6>Create Resume</h6>
+                    <div class="d-flex justify-content-between install mt-3">
+                      <span>
+                        Create a resume on Jobineer and get hired easily
+                      </span>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
+            </Link>
 
-            <div class="row">
-              <div class="col-md-4">
-                <div class="card p-3">
-                  <div class="d-flex flex-row mb-3">
-                    <img src={Findmatchjobs} alt="" width="40" />
-                  </div>
-                  <h6>Find Matched Jobs</h6>
-                  <div class="d-flex justify-content-between install mt-3">
-                    <span>
-                      Filter your job search accordingly and receive various
-                      offers
-                    </span>
+            <Link>
+              <div class="row">
+                <div class="col-md-4">
+                  <div class="card p-3">
+                    <div class="d-flex flex-row mb-3">
+                      <img src={Findmatchjobs} alt="" width="40" />
+                    </div>
+                    <h6>Find Matched Jobs</h6>
+                    <div class="d-flex justify-content-between install mt-3">
+                      <span>
+                        Filter your job search accordingly and receive various
+                        offers
+                      </span>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
+            </Link>
 
-            <div class="row">
-              <div class="col-md-4">
-                <div class="card p-3">
-                  <div class="d-flex flex-row mb-3">
-                    <img src={Easyapply} alt="" width="40" />
-                  </div>
-                  <h6>Easy Apply</h6>
-                  <div class="d-flex justify-content-between install mt-3">
-                    <span>
-                      Apply for jobs with easy with your registered profile
-                    </span>
+            <Link>
+              <div class="row">
+                <div class="col-md-4">
+                  <div class="card p-3">
+                    <div class="d-flex flex-row mb-3">
+                      <img src={Easyapply} alt="" width="40" />
+                    </div>
+                    <h6>Easy Apply</h6>
+                    <div class="d-flex justify-content-between install mt-3">
+                      <span>
+                        Apply for jobs with ease with your registered profile
+                      </span>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
+            </Link>
           </div>
         </div>
       </section>
